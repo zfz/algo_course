b5e4d859e2c98f2690f71f2569f020fa3bc4d403
zfz <zhoufangzhang@gmail.com>
2017-11-17 22:45:21 +0800

    Test part1 assignment6.1 two sum

---
+++ homework/part1/6_1_two_sum/two_sum.py
@@ -0,0 +1,48 @@
+import sys
+from collections import defaultdict
+from heapq import heappop, heappush
+import logging
+Logger = logging.getLogger()
+
+
+def setup_debug_logger():
+    handler = logging.StreamHandler()
+    Logger.addHandler(handler)
+    Logger.setLevel(logging.DEBUG)
+
+
+def read_lines(file_name):
+    d = {}
+    with open(file_name) as f:
+        for line in f:
+            d[int(line.strip())] = 1
+    return d
+
+
+def two_sum(t, d):
+    for x in d:
+        if t - x in d:
+            if x != (t-x):
+                Logger.debug("two_sum true : {0}".format(t))
+                return True
+    return False
+
+
+def count_two_sum(d):
+    c = 0
+    for t in range(-10000, 10001):
+        Logger.debug("two_sum: {0}".format(t))
+        if two_sum(t, d):
+            c += 1
+    return c
+
+
+if __name__ == "__main__":
+    setup_debug_logger()
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+        d = read_lines(file_name)
+        c = count_two_sum(d)
+        print("count_two_sum: {0}".format(c))
+    else:
+        print("Please provide file name!")
